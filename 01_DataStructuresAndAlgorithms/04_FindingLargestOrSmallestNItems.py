2c8d3ce7420d142582cf6ac48720a71e0342fa7b
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-05-03 17:59:52 +0900

    N 아이템의 최대 혹은 최소값 찾기.

---
+++ 01_DataStructuresAndAlgorithms/04_FindingLargestOrSmallestNItems.py
@@ -0,0 +1,26 @@
+import heapq
+
+# N 아이템의 최대 혹은 최소값 찾기
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+cheap = heapq.nsmallest(2, portfolio, key=lambda s: s['price'])
+print(type(cheap))
+print(cheap)
+
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+print(type(expensive))
+print(expensive)
+
+print(portfolio)
