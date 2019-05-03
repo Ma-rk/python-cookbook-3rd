39bfd70f41c4d776fcd9037a657f2de6f5d12f2d
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-05-03 19:34:32 +0900

    딕셔너리의 키를 여러 값에 매핑하기.

---
+++ 01_DataStructuresAndAlgorithms/07_KeepingDictionariesInOrder.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+
+d = OrderedDict()
+d['foo'] = 1
+d['bar'] = 2
+d['spam'] = 3
+d['grok'] = 4
+
+for key in d:
+    print(key, d[key])
+
+e = OrderedDict()
+e['fooo'] = 1
+e['barr'] = 2
+e['spamm'] = 3
+e['grokk'] = 4
+
+for key in e:
+    print(key, e[key])
