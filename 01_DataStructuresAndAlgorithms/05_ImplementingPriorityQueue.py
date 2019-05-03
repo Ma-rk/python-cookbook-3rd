59be731f5812e247a8d8898832498d62801249e8
Kim Hak Kyeol <k.mark.kr@gmail.com>
2019-05-03 18:50:23 +0900

    우선 순위 큐 구현.

---
+++ 01_DataStructuresAndAlgorithms/05_ImplementingPriorityQueue.py
@@ -0,0 +1,37 @@
+import heapq
+
+
+# 우선 순위 큐 구현
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+
+if __name__ == '__main__':
+    q = PriorityQueue()
+    q.push(Item('foo'), 1)
+    q.push(Item('bar'), 5)
+    q.push(Item('spam'), 4)
+    q.push(Item('grok'), 1)
+    
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
