1ff4d58e19226e81876aa9e463004f5d18c7a086
MacBookOfMark <mark_macbook@Mark-MBP-2019.local>
2019-09-10 19:27:57 +0900

    중첩 시퀀스 풀기.

---
+++ 04_IteratorsAndGenerators/14_FlatteningNestedSequence.py
@@ -0,0 +1,25 @@
+from collections.abc import Iterable
+
+
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+# Produces 1 2 3 4 5 6 7 8
+for x in flatten(items):
+    print(x)
+
+
+def flatten_with_no_yield_from(items, ignore_types=(str, bytes)):
+    # yield from 없이는 for 문을 하나 더 중첩해야 하며 코드가 더 복잡해진다
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            for i in flatten(x):
+                yield i
+        else:
+            yield x
